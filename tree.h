e7a978a8a3487d0cf3d4482bc152666bc7bc4027
foxido <git@forestfox.tk>
2022-11-20 18:57:01 +0300

    update headers

--- tree.h
+++ tree.h
@@ -61,10 +61,15 @@ namespace tree
     bool dfs_exec (tree_t *tree, walk_f pre_exec,  void *pre_param,
                                  walk_f in_exec,   void *in_param,
                                  walk_f post_exec, void *post_param);
+    bool dfs_exec (node_t *node, walk_f pre_exec,  void *pre_param,
+                                 walk_f in_exec,   void *in_param,
+                                 walk_f post_exec, void *post_param);
 
     void change_node (node_t *node, double val);
     void change_node (node_t *node, op_t   op);
     void change_node (node_t *node, unsigned char var);
+
+    void move_node (node_t *dest, node_t *src);
 
     void store (tree_t *tree, FILE *stream);
     tree::tree_err_t load (tree_t *tree, FILE *dump);
@@ -75,6 +80,12 @@ namespace tree
     tree::node_t *new_node (double val);
     tree::node_t *new_node (op_t   op);
     tree::node_t *new_node (unsigned char var);
+
+    void del_node (node_t *node);
+
+    void del_left   (node_t *node);
+    void del_right  (node_t *node);
+    void del_childs (node_t *node);
 }
 
 #endif //TREE_H
