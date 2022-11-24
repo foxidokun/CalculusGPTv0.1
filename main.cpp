de5570039860083d0f012057f762fa186a7eaee2
foxido <git@forestfox.tk>
2022-11-24 23:25:29 +0300

    Ad taylor to main

--- main.cpp
+++ main.cpp
@@ -28,13 +28,23 @@ int main()
     render::push_section    (&render, "Разбор первого выражения");
     render::push_subsection (&render, "Диффиринцирование");
 
-    tree::tree_t res = calc_diff (&tree, &render);
+    tree::tree_t res = calc_diff (&tree, 'x', &render);
 
     render::push_subsection (&render, "Упрощение");
 
     tree::simplify (&res, &render);
 
+    render::push_section (&render, "Разбор второго выражения");
+
+    tree::tree_t taylor_tree       = {};
+    tree::tree_t taylor_res        = {};
+    tree::load (&taylor_tree, fopen ("taylor.txt", "r"));
+
+    taylor_res = tree::taylor_series (&taylor_tree, 5, &render);
+
     tree::dtor(&tree);
     tree::dtor(&res);
+    tree::dtor(&taylor_tree);
+    tree::dtor(&taylor_res);
     render::render_dtor (&render);
 }
